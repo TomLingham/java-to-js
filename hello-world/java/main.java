d00906e685ba70e57d3dbedf5d9139bf9fd0ec2f
Tom Lingham <tjlingham@gmail.com>
2016-12-09 10:48:17 +1100

    Static var in java 'Hello World' example

--- hello-world/java/main.java
+++ hello-world/java/main.java
@@ -1,7 +1,9 @@
 class App {
 
+  static String my_string = "Hello World!";
+
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println(App.my_string);
   }
 
 }
